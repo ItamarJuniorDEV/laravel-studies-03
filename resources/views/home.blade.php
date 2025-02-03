8b305caede0c5202fca7fc58880e2780751ad74d
Itamar Junior <cdajuniorf@gmail.com>
2025-02-03 07:50:45 -0300

    feat: Iniciei a introdução a Blade Components.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.main_layout')
-
-@section('page_title', 'Home page')
-
 @section('content')
-<div class="container mt-5">
-  <div class="row">
-    <div class="col card p-5 bg-secundary text-center">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, quo.
-    </div>
-  </div>
-</div>
+
+<h3>Este texto faz parte da view</h3>
+
+{{-- renderizar o component --}}
+<x-my-component />
+
+{{-- renderizar o component que existe dentro da subpasta --}}
+<x-admin.admin-card />
+
+<h3>Este texto faz parte da view</h3>
+
 @endsection
